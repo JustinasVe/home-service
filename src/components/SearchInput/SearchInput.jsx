867090479d72b4eb2e5993bda05096f12264ac7d
unknown <justinasve@gmail.com>
2024-09-24 04:16:57 +0300

    feat: useState for search input value

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,12 +1,21 @@
 import styles from "./SearchInput.module.scss";
+import { useState } from "react";
 
 export const SearchInput = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <input
       className={styles.inputSearch}
       type="search"
       name="search"
       placeholder="Search"
+      value={searchQuery}
+      onChange={handleSearch}
     />
   );
 };

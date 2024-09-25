2890fc227c1b2c8c4b3869f2b95ad1fb88514839
unknown <justinasve@gmail.com>
2024-09-25 17:33:18 +0300

    feat: add TopBarLayout for managing app navigation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import { CategoryList } from "../../components/CategoryList/CategoryList";
 import { HeroSection } from "../../components/HeroSection/HeroSection";
-import { TopBar } from "../../components/TopBar/TopBar";
 
 export const HomePage = () => {
   return (
     <>
-      <TopBar />
       <HeroSection />
       <CategoryList />
     </>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,22 +3,29 @@ import { HomePage } from "../pages/HomePage/HomePage";
 import { ServicesPage } from "../pages/ServicesPage/ServicesPage";
 import { AboutUsPage } from "../pages/AboutUsPage/AboutUsPage";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
+import { TopBarLayout } from "../layouts/TopBarLayout/TopBarLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/services",
-    element: <ServicesPage />,
-  },
-  {
-    path: "/about-us",
-    element: <AboutUsPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
+    element: <TopBarLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/services",
+        element: <ServicesPage />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUsPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
   },
 ]);

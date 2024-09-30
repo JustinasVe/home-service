b7ee8a6a28d6a0b5952f343421f68b50bb894f64
unknown <justinasve@gmail.com>
2024-09-30 05:33:40 +0300

    feat: add BusinessCard and BusinessList components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
+import { BusinessList } from "../../components/BusinessList/BusinessList";
 import { CategoryList } from "../../components/CategoryList/CategoryList";
 import { HeroSection } from "../../components/HeroSection/HeroSection";
+import styles from "./HomePage.module.scss";
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
       <CategoryList />
+      <h2 className={styles.title}>Popular businesses</h2>
+      <BusinessList />
     </>
   );
 };

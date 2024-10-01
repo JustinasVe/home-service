7d88c6f6cbe4878c570eb2ee84ca4b4393b9a302
unknown <justinasve@gmail.com>
2024-10-01 04:23:20 +0300

    feat: add vertical category card and list components with dynamic routing for SearchCategoryPage

--- src/pages/SearchCategoryPage/SearchCategoryPage.jsx
+++ src/pages/SearchCategoryPage/SearchCategoryPage.jsx
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
+import styles from "./SearchCategoryPage.module.scss";
+import { VerticalCategoryList } from "../../components/VerticalCategoryList/VerticalCategoryList";
 
 export const SearchCategoryPage = () => {
   const { category } = useParams();
 
   return (
-    <h2>{category}</h2>
+    <div className={styles.container}>
+      <div className={styles.categoriesContainer}>
+        <VerticalCategoryList />
+      </div>
+      <div className={styles.businessContainer}>
+        <h2 className={styles.title}>{category}</h2>
+      </div>
+    </div>
   );
 };

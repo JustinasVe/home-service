dcba003f9d40d5ec777f6a430ae8ca252f26d5d4
unknown <justinasve@gmail.com>
2024-10-02 16:18:22 +0300

    feat: implement basic login and register page components

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,45 @@
 import { Link } from "react-router-dom";
 import { routes } from "../../router/router";
+import styles from "./LoginPage.module.scss";
+import { Input } from "../../components/Input/Input";
+import { Button } from "../../Button/Button";
+import { useState } from "react";
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState([""]);
+  const [password, setPassword] = useState([""]);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to={routes.register}>Don't have an account? Sign up</Link>
+    <div className={styles.container}>
+      <form onSubmit={handleLogin} className={styles.form}>
+        <h2 className={styles.title}>Login</h2>
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+          className={styles.input}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+          className={styles.input}
+        />
+        <Button type="submit">Log in</Button>
+        <div className={styles.link}>
+          <Link to={routes.register} className={styles.signUp}>
+            Don't have an account? Sign up
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,54 @@
 import { Link } from "react-router-dom";
 import { routes } from "../../router/router";
+import { Input } from "../../components/Input/Input";
+import { Button } from "../../Button/Button";
+import styles from "./RegisterPage.module.scss";
+import { useState } from "react";
 
 export const RegisterPage = () => {
+  const [email, setEmail] = useState([""]);
+  const [password, setPassword] = useState([""]);
+  const [confirmPassword, setConfirmPassword] = useState([""]);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to={routes.login}>Already have an account? Log in</Link>
+    <div className={styles.container}>
+      <form onSubmit={handleRegister} className={styles.form}>
+        <h2 className={styles.title}>Register</h2>
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+          className={styles.input}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+          className={styles.input}
+        />
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          required
+          className={styles.input}
+        />
+        <Button type="submit">Register</Button>
+        <div className={styles.link}>
+          <Link to={routes.login} className={styles.logIn}>
+            Already have an account? Log in
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
